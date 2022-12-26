79b916544d0984f38091045902cfdcb2f5d35589
Nick <nicholasconnors@hotmail.ca>
2022-12-26 13:42:18 -0500

    Add prompts for debug commands

--- Components/DebugCommands.cs
+++ Components/DebugCommands.cs
@@ -10,26 +10,53 @@ namespace ChristmasStory.Components
 {
 	internal class DebugCommands : MonoBehaviour
 	{
+		private static Key _debugKey = Key.I;
+		private static Key _warpKey = Key.O;
+
 		private readonly List<(Key key, Action action)> _debugCommands = new();
 		private readonly List<(Key key, Action action)> _debugWarpCommands = new();
 
+		private ScreenPrompt _debugPrompt;
+		private readonly List<ScreenPrompt> _debugPromptList = new();
+
+		private ScreenPrompt _warpPrompt;
+		private readonly List<ScreenPrompt> _warpPromptList = new();
+
 		public void Start()
 		{
-			RegisterDebugCommand(Key.Numpad1, ShipHandler.BlowUpShip);
-			RegisterDebugCommand(Key.Numpad2, HeldItemHandler.GivePlayerWarpCore);
-			RegisterDebugCommand(Key.Numpad3, HeldItemHandler.GivePlayerLantern);
-			RegisterDebugCommand(Key.Numpad9, Conditions.ResetAllConditions);
+			_debugPrompt = PromptUtils.AddPrompt("Debug Commands", PromptPosition.UpperRight, _debugKey);
+			_warpPrompt = PromptUtils.AddPrompt("Warp Commands", PromptPosition.UpperRight, _warpKey);
 
-			RegisterDebugWarpCommand(Key.Numpad1, () => WarpToPlanet(AstroObject.Name.TimberMoon, 100f)); // Esker
-			RegisterDebugWarpCommand(Key.Numpad2, () => WarpToPlanet(AstroObject.Name.CaveTwin, 180)); // Chert
-			RegisterDebugWarpCommand(Key.Numpad3, () => WarpToPlanet(AstroObject.Name.BrittleHollow, 350f)); // Riebeck
-			RegisterDebugWarpCommand(Key.Numpad4, WarpToGabbroIsland);
-			RegisterDebugWarpCommand(Key.Numpad5, WarpToFeldspar);
+			RegisterDebugCommand(Key.Numpad1, ShipHandler.BlowUpShip, "Blow up ship");
+			RegisterDebugCommand(Key.Numpad2, HeldItemHandler.GivePlayerWarpCore, "Get warp core");
+			RegisterDebugCommand(Key.Numpad3, HeldItemHandler.GivePlayerLantern, "Get lantern");
+			RegisterDebugCommand(Key.Numpad9, Conditions.ResetAllConditions, "Reset conditions");
+
+			RegisterDebugWarpCommand(Key.Numpad1, () => WarpToPlanet(AstroObject.Name.TimberMoon, 100f), "Warp to Esker");
+			RegisterDebugWarpCommand(Key.Numpad2, () => WarpToPlanet(AstroObject.Name.CaveTwin, 180), "Warp to Chert");
+			RegisterDebugWarpCommand(Key.Numpad3, () => WarpToPlanet(AstroObject.Name.BrittleHollow, 350f), "Warp to Riebeck");
+			RegisterDebugWarpCommand(Key.Numpad4, WarpToGabbroIsland, "Warp to Gabbro");
+			RegisterDebugWarpCommand(Key.Numpad5, WarpToFeldspar, "Warp to Feldspar");
 		}
 
 		public void Update()
 		{
-			if (Keyboard.current[Key.P].isPressed)
+			if (Keyboard.current[_debugKey].wasPressedThisFrame)
+			{
+				foreach (var prompt in _debugPromptList)
+				{
+					prompt.SetVisibility(true);
+				}
+			}
+			else if (Keyboard.current[_debugKey].wasReleasedThisFrame)
+			{
+				foreach (var prompt in _debugPromptList)
+				{
+					prompt.SetVisibility(false);
+				}
+			}
+
+			if (Keyboard.current[_debugKey].isPressed)
 			{
 				foreach (var (key, action) in _debugCommands)
 				{
@@ -40,7 +67,22 @@ namespace ChristmasStory.Components
 				}
 			}
 
-			if (Keyboard.current[Key.O].isPressed)
+			if (Keyboard.current[_warpKey].wasPressedThisFrame)
+			{
+				foreach (var prompt in _warpPromptList)
+				{
+					prompt.SetVisibility(true);
+				}
+			}
+			else if (Keyboard.current[_warpKey].wasReleasedThisFrame)
+			{
+				foreach (var prompt in _warpPromptList)
+				{
+					prompt.SetVisibility(false);
+				}
+			}
+
+			if (Keyboard.current[_warpKey].isPressed)
 			{
 				foreach (var (key, action) in _debugWarpCommands)
 				{
@@ -50,10 +92,23 @@ namespace ChristmasStory.Components
 					}
 				}
 			}
+
+			var buttonsPressed = Keyboard.current[_debugKey].isPressed || Keyboard.current[_warpKey].isPressed;
+			_warpPrompt.SetVisibility(!buttonsPressed);
+			_debugPrompt.SetVisibility(!buttonsPressed);
 		}
 
-		private void RegisterDebugCommand(Key key, Action action) => _debugCommands.Add((key, action));
-		private void RegisterDebugWarpCommand(Key key, Action action) => _debugWarpCommands.Add((key, action));
+		private void RegisterDebugCommand(Key key, Action action, string description)
+		{
+			_debugCommands.Add((key, action));
+			_debugPromptList.Add(PromptUtils.AddPrompt(description, PromptPosition.UpperRight, key));
+		}
+
+		private void RegisterDebugWarpCommand(Key key, Action action, string description)
+		{
+			_debugWarpCommands.Add((key, action));
+			_warpPromptList.Add(PromptUtils.AddPrompt(description, PromptPosition.UpperRight, key));
+		}
 
 		private static void WarpToPlanet(AstroObject.Name planetName, float offset)
 		{
---
+++ Utility/PromptUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+namespace ChristmasStory.Utility
+{
+	internal static class PromptUtils
+	{
+		public static ScreenPrompt AddPrompt(string text, PromptPosition position, Key key)
+		{
+			Enum.TryParse(key.ToString().Replace("Digit", "Alpha").Replace("Numpad", "Alpha"), out KeyCode keyCode);
+
+			return AddPrompt(text, position, keyCode);
+		}
+
+		public static ScreenPrompt AddPrompt(string text, PromptPosition position, KeyCode keyCode)
+		{
+			var texture = ButtonPromptLibrary.SharedInstance.GetButtonTexture(keyCode);
+			var sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100, 0, SpriteMeshType.FullRect, Vector4.zero, false);
+			sprite.name = texture.name;
+
+			var prompt = new ScreenPrompt(text, sprite);
+			Locator.GetPromptManager().AddScreenPrompt(prompt, position, false);
+
+			return prompt;
+		}
+	}
+}
